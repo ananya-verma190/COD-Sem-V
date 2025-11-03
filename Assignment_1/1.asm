f2ef3a939abe1c75abb0415c9f416df4caf3c40c
Ananya Verma <ananyaverma190@gmail.com>
2025-11-03 22:33:56 +0530

    Added code

--- Assignment_1/1.asm
+++ Assignment_1/1.asm
@@ -1,1 +1,24 @@
 # Search for a character and replace it with a special character
+
+.data
+str: .string "abcdct"
+character: .byte 99        # ASCII value for 'c'
+replace: .byte 63          # ASCII value for '?'
+
+.text
+la x7, str
+la x5, character
+la x6, replace
+
+lb x5, 0(x5)                # x5 = c
+lb x6, 0(x6)                # x6 = ?
+
+loop:
+lb x10, 0(x7)
+beq x10, x0, end            # check is string has ended
+bne x10, x5, skip
+sb x6, 0(x7)
+skip: 
+addi x7, x7, 1
+beq x0, x0, loop
+end: nop
